db567db3b0d6f82111795e10476d99dd0b0966e4
Monzon3 <sergio.monzon3@gmail.com>
2022-11-05 01:25:23 +0100

    Import finished. Pending to verify all data have been properly copied into new database

--- 01_ImportDatabase/databaseImporter.py
+++ 01_ImportDatabase/databaseImporter.py
@@ -1,11 +1,11 @@
-'''
-After the new database and its tables have been created, export values from the original Access database
+''' After the new database and its tables have been created, export values from the original Access database
 into a file named 'Peliculas.xlsx' and store it in \Resources. 
-Then copy the newly created 'Peliculas.db' file into this path (\01_ImportDatabase) 
+
+Then, copy the newly created 'Peliculas.db' file into /01_ImportDatabase 
 and execute this script to obtain all unique values from the original fields 'Disco', 'Calidad', 
-'Idioma' and 'Pais'. 
-Then import those values into the new database.
-'''
+'Idioma' and 'Pais' and import them values into the new database.
+
+Finally, the genres table is also populated with the values from the .json file 'ListGenres'.'''
 
 from configparser import ConfigParser
 import dbConnector
@@ -13,23 +13,24 @@ import json
 import pandas as pd
 import sqlite3 as sql
 
-def get_individual_values(col, dataFrame):
-    '''
-        - col: Name of the column in the original Access database to work with
-        - dataFrame: Full original Access database, in a Pandas DataFrame structure
-    '''
+def get_unique_values(col, dataFrame):
+    ''' This function will obtain the unique values from a specific column in the original database
+    to populate the new 'Calidad', 'Disco', 'Idioma' and 'Pais' tables in the new database
+
+    - col: Name of the column in the original Access database to work with
+    - dataFrame: Full original Access database, in a Pandas DataFrame structure'''
+
     # Obtain different values from 'col' column to insert into the new 'col' table
     series = dataFrame[-dataFrame.duplicated(col)][col]
 
    # 'IdiomaAudio' (Language) values are processed differently 
     if col == 'IdiomaAudio': 
-        # This values will be updated for the future versions
-        series[series=='Maya'] = 'May' 
-        series[series=='Var'] = 'Varios' 
+        # This values will be updated for the future version of the database
+        series = series.apply(lambda x: x.replace('Maya', 'May'))
+        series = series.apply(lambda x: x.replace('Var', 'Varios'))
 
         # Latin is added (there is one movie with subtitles in Latin)
-        s_aux = pd.Series(['Lat'])
-        series = pd.concat([series, s_aux])
+        series = pd.concat([series, pd.Series(['Lat'])])
 
         # Delete double-values and finish off
         series = series[-series.str.find('-')>0]
@@ -40,13 +41,15 @@ def get_individual_values(col, dataFrame):
 
 
 def update_database(series, table, col):
-    '''
-        - series: Series of values to insert into the new dataframe's tables
-        - table: Name of the table in which to insert these above mentioned values 
-        - col: Name of the column, within the table, in which to insert values
+    ''' This function will populate the tables 'Calidad', 'Disco', 'Idioma' and 'Pais' in the new database
+    with the unique values obtained from the original database
+    
+    - series: Series of values to insert into the new dataframe's tables
+    - table: Name of the table in which to insert these above mentioned values 
+    - col: Name of the column, within the table, in which to insert values
     '''
 
-    # Full languages names need to be added so the NOT NULL constraint is met
+    # Full languages' names need to be added so the NOT NULL constraint is met
     if table == 'Idioma':
         with open(config.get('Aux_files', 'languages_list'), encoding = 'utf-8') as f:
             lang_list = json.load(f)
@@ -56,19 +59,20 @@ def update_database(series, table, col):
             if table == 'Idioma':
                 sql_query = 'INSERT INTO ' + table + ' (' + col + ''', IdiomaCompleto) 
                             VALUES (\'''' + val + '\', \'' + lang_list[val] + '\')'
+                sql_query = f'''INSERT INTO {table} ({col}, IdiomaCompleto)
+                            VALUES (\'{val}\', \'{lang_list[val]}\')'''
             else:
-                sql_query = 'INSERT INTO ' + table + ' (' + col + ') VALUES (\'' + val + '\')'
+                sql_query = f'INSERT INTO {table} ({col}) VALUES (\'{val}\')'
             
             db.execute(sql_query)
             conn.commit()
         
         except sql.Error as error:
-            print(f'Error while updating the table {table}: ', error)
+            print(f'Error while updating the table {table} using:\n {sql_query}: ', error)
 
 
 def update_genres():
-    '''
-    This function will populate the table 'Generos' included in the database using the 
+    ''' This function will populate the table 'Generos' included in the database using the 
     .json file 'ListGenres', where all options should be included.
     '''
 
@@ -77,13 +81,13 @@ def update_genres():
 
     for i in genres.keys():
         try:
-            sql_query = 'INSERT INTO Genero (Categoria, Nombre) VALUES (\'' + genres[i]['Category'] + '\', \'' + genres[i]['Name'] + '\')'
-            
+            sql_query = f'''INSERT INTO Genero (Categoria, Nombre) VALUES 
+                        (\'{genres[i]['Category']}\', \'{genres[i]['Name']}\')'''            
             db.execute(sql_query)
             conn.commit() 
 
         except sql.Error as error:
-            print(f'Error while updating the table \'Genres\': ', error)
+            print(f'Error while updating the table \'Genres\' using:\n {sql_query}: ', error)
 
 
 if __name__ == '__main__':
@@ -99,17 +103,21 @@ if __name__ == '__main__':
     movie_database = pd.read_excel(excel_path)
 
     # Get unique values for the following columns to populate the new tables in the database
-    disc = get_individual_values('Disco', movie_database)
-    quality = get_individual_values('Calidad', movie_database)
-    lang = get_individual_values('IdiomaAudio', movie_database)
-    country = get_individual_values('Pais', movie_database)
+    disc = get_unique_values('Disco', movie_database)
+    quality = get_unique_values('Calidad', movie_database)
+    lang = get_unique_values('IdiomaAudio', movie_database)
+    country = get_unique_values('Pais', movie_database)
 
-    # Import the values into the new database
+    # Import the unique values into the new database
     update_database(disc, 'Disco', 'Disco')
     update_database(quality, 'Calidad', 'Calidad')
     update_database(lang, 'Idioma', 'IdiomaAbreviado')
     update_database(country, 'Pais', 'Pais')
     update_genres()
+
+    print('\n')
+    print('If no errors have been shown, all values have been correctly imported')
+    print('  into the tables \'Calidad\', \'Disco\', \'Idioma\' and \'Pais\' in the new database')
 
     db.close()
     conn.close()
--- 02_ImportData/dataImporter.py
+++ 02_ImportData/dataImporter.py
@@ -1,11 +1,14 @@
-'''
-After the new database structure has been created and the reference tables populated, 
+''' After the new database structure has been created and the reference tables populated, 
 the real data from the Access database is imported into the new "Main" table. 
+
 To do this, all reference tables should be looked up in order to exchange the values from
-the database for the new corresponding IDs.
+the original database for the new corresponding IDs.
+
+After this is done, the tables Audio_in_file, Genre_in_file and Subs_in_file
+are populated using the information from the original database and the reference tables.
+
 Before running this script, copy the database from /01_ImportDatabase/Peliculas.db into
-/test.db
-'''
+/test.db.'''
 
 from configparser import ConfigParser
 import dbConnector
@@ -14,7 +17,11 @@ import sqlite3 as sql
 
 
 def import_df_to_db(dataFrame, connector, cursor):
+    ''' After finding the new ID values for the fields 'Calidad', 'Disco' and 'Pais'
+    the whole database from the Excel file is imported into the new SQL database.'''
+
     for i in range(dataFrame.shape[0]):
+        # ' are replace by '' so SQL is able to process them properly
         tit = dataFrame.loc[i, 'Titulo'].replace("\'","\'\'")
         origTit = dataFrame.loc[i, 'TituloOriginal'].replace("\'","\'\'")
         disc = dataFrame.loc[i, 'Disco']
@@ -34,21 +41,110 @@ def import_df_to_db(dataFrame, connector, cursor):
             cursor.execute(sql_query)
             connector.commit()
         
-        except sql.error as error:
+        except sql.Error as error:
             print(f'Error during the execution of {sql_query}', error)
+
+
+def import_genres(movie_id, value, connector, cursor):
+    ''' This function will read all values from 'Genero' column
+    in the Excel database, split them using the ',' character in the movies
+    with more than one genre and then populate the table Genero_in_file with the genres found.'''
+    
+    # Obtain corresponding GeneroID from the new 'Genero' table for each genre in 'value'
+    if value != '-':
+        genres = value.split(',')[0:-1]    # value always ends with ',' so the last element is not needed
+
+        for i in genres:
+            # All genres have a category defined between brackets that is not needed for now
+            pos1 = i.find('[')
+            pos2 = i.find(']')
+            category = i[(pos1 + 1):pos2]
+            genre = i[(pos2 + 2):]
+
+            # Some genres have changed in the new database, so to find them:
+            if genre == 'Palma de Oro': genre = 'Cannes - Palma de Oro'
+            if genre == 'Mejor película' and category == 'Premios - Goya': 
+                genre = 'Goya - Mejor película'
+            if genre == 'Mejor director': genre = 'Oscars - Mejor director'
+            if genre == 'Mejor extranjera': genre = 'Oscars - Mejor extranjera'
+            if genre == 'Mejor fotografía': genre = 'Oscars - Mejor fotografía'
+            if genre == 'Mejor película' and category == 'Premios - Óscars':
+                genre = 'Oscars - Mejor película'
+
+            sql_query = f'SELECT id FROM Genero WHERE Nombre = \'{genre}\''
+            res = db.execute(sql_query).fetchone()
             
-    print('All data imported into the new database')
+            if res != None:
+                sql_query = f'''INSERT INTO Genero_in_file (pelicula_id, genero_id) 
+                            VALUES ({movie_id}, {res[0]})'''
+                try:
+                    cursor.execute(sql_query)
+                    connector.commit()
+                
+                except sql.Error as error:
+                    print(f'Error during the execution of {sql_query}', error)
+
+            else:
+                print(f'{sql_query} found nothing')
+
+
+def import_languages(movie_id, category, value, connector, cursor):
+    ''' This function will read all values from 'IdiomaAudio' and 'IdiomSubtitulos' columns
+    in the Excel database, split them using the '-' character in the movies
+    with two languages in 'Audio' or 'Subs' and then populate the tables 
+    Audio_in_file and Subs_in_file with the languages found.'''
+
+    id = []
+    # To match the new values in the database these two have to be changed,
+    # because in the original database they were 'May' and 'Var', instead of 'Maya' and 'Varios'
+    # (see function 'get_individual_values' in /01_ImportDatabase/databaseImporter.py for reference)
+    if value == 'Maya': 
+        value = 'May'
+    
+    if value == 'Var':
+        value = 'Varios'
+
+    # Obtain corresponding LanguageID from the new 'Idioma' table for each language in 'value'
+    if value != '-':
+        if value.find('-') != -1:
+            [lang1, lang2] = value.split('-')
+            sql_query = f'SELECT id FROM Idioma WHERE IdiomaAbreviado = \'{lang1}\''
+            id.append(db.execute(sql_query).fetchone()[0])
+
+            sql_query = f'SELECT id FROM Idioma WHERE IdiomaAbreviado = \'{lang2}\''
+            id.append(db.execute(sql_query).fetchone()[0])
+
+        elif value.find('-') == -1:
+            sql_query = f'SELECT id FROM Idioma WHERE IdiomaAbreviado = \'{value}\''
+            id.append(db.execute(sql_query).fetchone()[0])
+
+    # Populate Audio_in_file and Subs_in_file with the obtained values
+    for i in id:
+        if category == 'Audio':
+            sql_query = f'INSERT INTO Audio_in_file (pelicula_id, idioma_id) VALUES ({movie_id}, {i})'
+
+        elif category == 'Subs':
+            sql_query = f'INSERT INTO Subs_in_file (pelicula_id, idioma_id) VALUES ({movie_id}, {i})'
+
+        try:
+            cursor.execute(sql_query)
+            connector.commit()
+        
+        except sql.Error as error:
+            print(f'Error during the execution of {sql_query}', error)
 
 
 def obtainID(field, value):
-    '''
-    - field: Name of the table in which to look into (which is the same as the column's name within that table)
-    - value: Value to look for in the database and obtain its ID
-    '''
-    sql_query = f'SELECT id FROM {field} WHERE {field} = \'{value}\''
-    equivalent = db.execute(sql_query).fetchone()
+    ''' Function to obtain the ID for a given 'value' in a given table.
 
-    return equivalent[0]
+    - field: Name of the table in which to look into (which is the same as the column's name within that table)
+    - value: Value to look for in the database and obtain its ID.'''
+
+    table = field
+    sql_query = f'SELECT id FROM {table} WHERE {field} = \'{value}\''
+    id = db.execute(sql_query).fetchone()
+
+    return id[0]
 
 
 if __name__ == '__main__':
@@ -60,7 +156,7 @@ if __name__ == '__main__':
     excel_path = config.get('Aux_files', 'excel_database')
     movie_database = pd.read_excel(excel_path)
 
-    # Connect with the database located in [Paths] 'test_database' from .ini file
+    # # Connect with 'test_database' which is in [Paths] section from .ini file
     [conn, db] = dbConnector.connect_to_db('test_database')
 
     # Obtain IDs from database for old 'Pais', 'Disco' and 'Calidad' values
@@ -72,6 +168,22 @@ if __name__ == '__main__':
 
     # Import dataFrame with updated 'Pais', 'Disco' and 'Calidad' values into SQL
     import_df_to_db(movie_database, conn, db)
+
+    # After the old database is imported into the new one, the following can be done without breaking
+    # any constraints related with FOREIGN KEYS.
+    for i in range(movie_database.shape[0]):
+        # Obtain Audios and Subs from 'movie_database' for each film 
+        # and generate 'Audio_in_file' and 'Subs_in_file' tables
+        import_languages(movie_database.loc[i, 'Id'], 'Audio', 
+                            movie_database.loc[i, 'IdiomaAudio'], conn, db)
+
+        import_languages(movie_database.loc[i, 'Id'], 'Subs', 
+                            movie_database.loc[i, 'IdiomaSubtitulos'], conn, db)
+
+        # Do the same with genres
+        import_genres(movie_database.loc[i, 'Id'], movie_database.loc[i, 'Genero'], conn, db)
+
+    print('All data imported into the new database')
 
     # Export values to Excel to be able to compare with the original ones to verify the process
     # See macro inside DataChecker.xlsm to verify that the imported data is the same as the original
